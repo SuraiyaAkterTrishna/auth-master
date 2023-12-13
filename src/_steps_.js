87f0ac9c1912ea997d4f3482bba56bd2c7b089a8
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-12-13 17:55:18 +0600

    context API

---
+++ src/_steps_.js
@@ -0,0 +1,13 @@
+/**
+ * CONTEXT API
+ * 1. Create context (export)
+ * 2. create provider
+ * 3. set context value
+ * 4. to access the value: useContext hook
+*/
+
+/**
+ * Share CONTEXT API
+ * 1. main.jsx set AuthProvider
+ * 2. inside provider access children props and then use
+*/
---
+++ src/providers/AuthProviders.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import { createContext } from "react";
+
+export const AuthContext = createContext(null);
+
+const AuthProviders = ({children}) => {
+    
+    const user = {displayName: 'Sagor Nodi'};
+
+    return (
+        <AuthContext.Provider value={user}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProviders;
