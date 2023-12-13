e20cc4f72a60a66412502c29a42b602b971d069b
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-12-13 16:59:44 +0600

    header done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
-
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div>
-            <h3>This is Header</h3>
-        </div>
-    );
+  return (
+    <div>
+      <div className="navbar bg-primary text-primary-content">
+        <Link className="btn btn-ghost normal-case text-xl text-white">Auth Master</Link>
+        <Link className="btn btn-ghost normal-case text-xl text-white" to="/">Home</Link>
+        <Link className="btn btn-ghost normal-case text-xl text-white" to="/login">Login</Link>
+        <Link className="btn btn-ghost normal-case text-xl text-white" to="/register">Register</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-undef */
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: ["light"],
   },
   plugins: [require("daisyui")],
 }
-
